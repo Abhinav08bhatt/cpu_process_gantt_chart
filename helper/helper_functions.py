f307c061011b4076f534ba9e590cf2fe87f4e362
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2026-09-22 06:09:26 +0530

    bug fixed

--- helper/helper_functions.py
+++ helper/helper_functions.py
@@ -1,7 +1,6 @@
 def remove_duplicate(p):
     checker = []
-    for i in range (len(set(p))):
-        if p[i] in checker:
-            continue
-        checker.append(p[i])
+    for item in p:
+        if item not in checker:
+            checker.append(item)
     return checker
